18c686a9aa9144f70a1e8bd189d589e8351b5641
4kari <mypasshidden@gmail.com>
2021-08-27 21:16:23 +0700

    update database skripsi

--- lay_skripsi.sql
+++ lay_skripsi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2021 at 11:00 AM
+-- Generation Time: Aug 27, 2021 at 04:15 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.4.19
 
