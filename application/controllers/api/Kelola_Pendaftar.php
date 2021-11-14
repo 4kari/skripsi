c58213b8ad01c70b405011b633a86a00860edc96
4kari <mypasshidden@gmail.com>
2021-11-14 20:40:38 +0700

    tambah api kelola pendaftara u/ koordinator

---
+++ application/controllers/api/Kelola_Pendaftar.php
@@ -0,0 +1,91 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+class Kelola_Pendaftar extends REST_Controller{
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Validasi_model','mValidasi');
+    }
+    public function index_get(){
+        $Validasi = $this->mValidasi->getPendaftar();
+        if ($Validasi){
+            $this->response([
+                'status' => true,
+                'data' =>$Validasi
+            ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'data tidak ditemukan'
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function index_delete(){
+        $id = $this->delete('id');
+        if ($id == null){
+            var_dump($id);
+            $this->response([
+                'status' => false,
+                'message' => 'tambahkan id untuk hapus'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+            if ($this->mValidasi->deleteValidasi($id)>0){
+                //ok
+                $this->response([
+                    'status' => true,
+                    'message' => 'terhapus'
+                ], REST_Controller::HTTP_NO_CONTENT);
+            }
+            else{
+                $this->response([
+                    'status' => false,
+                    'message' => 'id tidak ditemukan'
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }          
+        }
+    }
+    public function index_post(){
+        $data=[
+            'id_skripsi' => $this->post('id_skripsi'),
+            'tipe' => $this->post('tipe')
+        ];
+        
+        if ($this->mValidasi->createValidasi($data)>0){
+            $this->response([
+                'status' => true,
+                'message' => 'Validasi baru berhasil dibuat'
+            ], REST_Controller::HTTP_CREATED);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal menambahkan Validasi baru'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+    public function index_put(){
+        $id=$this->put('id');
+        $data=[
+            'pembimbing_1' => $this->put('pembimbing_1'),
+            'pembimbing_2' => $this->put('pembimbing_2'),
+            'penguji_1' => $this->put('penguji_1'),
+            'penguji_2' => $this->put('penguji_2'),
+            'penguji_3' => $this->put('penguji_3')
+        ];
+
+        if ($this->mValidasi->updateValidasi($data,$id)>0){
+            $this->response([
+                'status' => true,
+                'message' => 'Validasi berhasil di perbarui'
+            ], REST_Controller::HTTP_NO_CONTENT);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Gagal memperbarui data'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+}
--- application/models/Validasi_model.php
+++ application/models/Validasi_model.php
@@ -9,6 +9,33 @@ class Validasi_model extends CI_Model{
     public function getValidasiBySkripsi($id){
         return $this->db->get_where('validasi',['id_skripsi'=>$id])->result_array();
     }
+    public function getPendaftar(){
+        $validasi=$this->getValidasi();
+        $data = [];
+        foreach($validasi as $v){        
+            $v['data_skripsi']=$this->db->get_where('skripsi',['id'=>$v['id_skripsi']])->row_array(); //dapat data skripsi tiap validasi
+            $jadwal = json_decode($this->curl->simple_get('http://localhost/microservice/penjadwalan/api/Jadwal/',array('id_skripsi'=>$v['id_skripsi']), array(CURLOPT_BUFFERSIZE => 10)),true);
+            // $jadwal = json_decode($this->curl->simple_get('http://10.5.12.47/penjadwalan/api/Jadwal/',array('id_skripsi'=>$v['id_skripsi']), array(CURLOPT_BUFFERSIZE => 10)),true);
+            
+            // var_dump($jadwal);
+            // die();
+            $found=FALSE;
+            if(!$v['pembimbing_1'] || !$v['pembimbing_2']){
+                $found=TRUE;
+            }
+            if($jadwal && $found===FALSE){
+                foreach($jadwal as $j){
+                    if($j['tipe']==$v['tipe']-1){
+                        $found=TRUE;
+                    }
+                }
+            }
+            if($found!==TRUE){
+                array_push($data,$v);
+            }
+        }
+        return $data;
+    }
     public function deleteValidasi($id){
         //data master tidak bisa dihapus
         $this->db->delete('Validasi', ['id' => $id]);
