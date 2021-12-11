bc23aa30cf11ba0f58b90a4158b63b14d2c258b8
4kari <mypasshidden@gmail.com>
2021-12-11 12:44:25 +0700

    update fitur penilaian post

--- application/controllers/api/Penilaian.php
+++ application/controllers/api/Penilaian.php
@@ -10,7 +10,7 @@ class Penilaian extends REST_Controller{
         $this->load->model('Penilaian_model','mPenilaian');
     }
     public function index_get(){
-        $id = $this->get('id');
+        $id = $this->get('id_skripsi');
         $penilai = $this->get('penilai');
         $Penilaian = $this->mPenilaian->getPenilaian($id,$penilai);
         if ($Penilaian){
@@ -25,40 +25,13 @@ class Penilaian extends REST_Controller{
             ], REST_Controller::HTTP_NOT_FOUND);
         }
     }
-
-    public function index_delete(){
-        $id = $this->delete('id');
-        if ($id == null){
-            var_dump($id);
-            $this->response([
-                'status' => false,
-                'message' => 'tambahkan id untuk hapus'
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        } else {
-            if ($this->mPenilaian->deletePenilaian($id)>0){
-                //ok
-                $this->response([
-                    'status' => true,
-                    'message' => 'terhapus'
-                ], REST_Controller::HTTP_NO_CONTENT);
-            }
-            else{
-                $this->response([
-                    'status' => false,
-                    'message' => 'id tidak ditemukan'
-                ], REST_Controller::HTTP_BAD_REQUEST);
-            }          
-        }
-    }
     public function index_post(){
         $data=[
             'id_skripsi' => $this->post('id_skripsi'),
             'penilai' => $this->post('penilai'),
-            'kode_penilaian' => $this->post('kode_penilaian'),
-            'nilai' => $this->post('nilai')
+            'kode_penilaian' => $this->post('kode_penilaian')
         ];
-        
-        if ($this->mPenilaian->createPenilaian($data)>0){
+        if ($this->mPenilaian->createPenilaian($data,$this->post('nilai'))>0){
             $this->response([
                 'status' => true,
                 'message' => 'Penilaian baru berhasil dibuat'
@@ -67,27 +40,6 @@ class Penilaian extends REST_Controller{
             $this->response([
                 'status' => false,
                 'message' => 'Gagal menambahkan Penilaian baru'
-            ], REST_Controller::HTTP_BAD_REQUEST);
-        }
-    }
-    public function index_put(){
-        $id=$this->put('id');
-        $data=[
-            'id_skripsi' => $this->put('id_skripsi'),
-            'penilai' => $this->put('penilai'),
-            'kode_penilaian' => $this->put('kode_penilaian'),
-            'nilai' => $this->put('nilai')
-        ];
-
-        if ($this->mPenilaian->updatePenilaian($data,$id)>0){
-            $this->response([
-                'status' => true,
-                'message' => 'Penilaian berhasil di perbarui'
-            ], REST_Controller::HTTP_NO_CONTENT);
-        } else {
-            $this->response([
-                'status' => false,
-                'message' => 'Gagal memperbarui data'
             ], REST_Controller::HTTP_BAD_REQUEST);
         }
     }
--- application/models/Penilaian_model.php
+++ application/models/Penilaian_model.php
@@ -1,18 +1,16 @@
 <?php
 class Penilaian_model extends CI_Model{
     public function getPenilaian($id=null,$penilai=null){
-        return $this->db->get_where('Penilaian', ['id' => $id, 'penilai' => $penilai])->result_array();
+        return $this->db->get_where('Penilaian', ['id_skripsi' => $id, 'penilai' => $penilai])->result_array();
     }
-    public function deletePenilaian($id){
-        $this->db->delete('Penilaian', ['id' => $id]);
-        return $this->db->affected_rows();
-    }
-    public function createPenilaian($data){
-        $this->db->insert('Penilaian',$data);
-        return $this->db->affected_rows();
-    }
-    public function updatePenilaian($data,$id){
-        $this->db->update('Penilaian', $data, ['id' => $id]);
+    public function createPenilaian($data,$nilai){
+        $penilaian = $this->db->get_where('Penilaian',$data)->row_array();
+        if($penilaian){
+            $this->db->update('Penilaian', array('nilai'=>$nilai), $data);
+        }else{
+            $data['nilai']=$nilai;
+            $this->db->insert('Penilaian',$data);
+        }
         return $this->db->affected_rows();
     }
 }
